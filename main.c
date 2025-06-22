8afafbe5de534684cc1f13e2473be7f04715839f
Shefilkhan <spathan8633@conestogac.on.ca>
2025-06-21 21:17:14 -0400

    Main file updated with functions implementations

--- main.c
+++ main.c
@@ -5,39 +5,128 @@
   FIRST VERSION : 2025-06-15
   DESCRIPTION   : This file contains the main menu interface that allows the user to select and run different math operations
   (add, subtract, multiply, divide) and testing. All actions are logged to a file using the custom logger module.
+
+  CITATION:
+    TITLE       : Input validation using scanf_s and clearing stdin
+    AUTHOR      : Microsoft Developer Network (MSDN)
+    DATE        : 2024-06-01
+    AVAILABILITY: https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/scanf-s-scanf-s-l-wscanf-s-wscanf-s-l
 */
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"logger.h"
+#include"test_harness.h"
 
+#define MAX_NAME_LEN 65
+
+/*
+  FUNCTION      : viewLogFile
+  DESCRIPTION   : Opens and displays the contents of the specified log file.
+  PARAMETERS    : const char* filename - Name of the file to open and read
+  RETURNS       : void
+ 
+  CITATION:
+    TITLE       : Read a file character by character in C
+    AUTHOR      : C Library Reference + TutorialsPoint
+    DATE        : 2024-06-01
+    AVAILABILITY: https://www.geeksforgeeks.org/c-program-read-contents-file/ 
+ */
+void viewLogFile(const char* filename)
+{
+	FILE* fp = fopen(filename, "r");
+
+	if (!fp)
+	{
+		printf("Could not open log file: %s\n", filename);
+		return;
+	}
+
+	printf("\n--- %s ---\n", filename);
+
+	char c;
+	while ((c = fgetc(fp)) != EOF)
+	{
+		putchar(c);
+	}
+
+	fclose(fp);
+}
+
+/*
+  FUNCTION      : main
+  DESCRIPTION   : Entry point of the program. Handles user interaction, test execution, and logging activities.
+  PARAMETERS    : None
+  RETURNS       : int - Exit code (0 if successful)
+ */
 int main(void)
 {
-	int Choice; 
+	int Choice;
+	int testIndex;
 
-	do {
-		printf("\nMain Menu\n");
-		printf("1. Run individual test cases\n");
-		printf("2. Run all tests in the harness\n");
-		printf("3. View log files\n");
+	// Initialize the main log to track program startup
+	initLogger("program.log");
+	logMessage("INFO", "Program started");
+	closeLogger();
+
+	//initialize test specific log for capturing test results
+	initLogger("test.log");
+
+	while (1)
+	{
+		// Print main menu directly inside main
+		printf("\n==== MAIN MENU ====\n");
+		printf("1. Run all tests\n");
+		printf("2. Run test by number\n");
+		printf("3. View logs\n");
 		printf("4. Exit\n");
 		printf("Please Enter you choice:");
-		scanf_s("%d", &Choice);
+
+		if (scanf_s("%d", &Choice) != 1); {
+			printf("Invalvalid input. Try again.\n");
+			while (getchar() != '\n'); // this will clear input buffer
+			continue;
+		}
 
 		switch (Choice) {
 		case 1:
-			printf("Run individual test cases (function not implemented yet)\n");
+			logMessage("INFO", "Running all tests");
+			runAllTests();
 			break;
 
 		case 2:
-			printf("Run all tests in the harness (function not implemented yet)\n");
+		{
+			int total = getTotalTests();
+			printf("Select a test to run:\n");
+
+			for (int i = 0; i < total; ++i)
+			{
+				printf("  %d. %s\n", i + 1, getTestName(i));
+			}
+
+			printf("Enter test number: ");
+			if (scanf_s("%d", &testIndex) != 1 || testIndex < 1 || testIndex > total)
+			{
+				printf("Invalid selection.\n");
+				while (getchar() != '\n');
+				break;
+			}
+
+			logMessage("INFO", "Running test %d: %s", testIndex, getTestName(testIndex - 1));
+			runTestByIndex(testIndex - 1);
 			break;
+		}
 
 		case 3:
-			printf("View log files (function not implemented yet)\n");
+			viewLogFile("program.log");
+			viewLogFile("test.log");
 			break;
 
 		case 4:
-			printf("Exit\n");
-			break;
-
+			logMessage("INFO", "Program exited");
+			closeLogger();
+			return 0;
+			
 		default:
 			printf("Invalid Choice\n");
 
